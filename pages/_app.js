6754179e132aea84b394f11d76062ce9c228bd24
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-05-17 09:18:59 +0600

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css';
 
 // import components
-import Layout from "./components/layout/Main";
+import Layout from "../components/layout/Main";
 
 export default function App({ Component, pageProps }) {
   return (
